9934157e9eb51bfba2b16ecd7a117a474adf8f82
thisconnect <p7tryk@gmail.com>
2021-01-13 16:07:07 +0100

    demo1 rc3

--- prezentacja/demo1/demo1.sh
+++ prezentacja/demo1/demo1.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 DIRECTORY="demo1"
 
-cd ~/
-if [ ! -d "$DIRECTORY" ]; then
+
+if [ ! -d "~/$DIRECTORY" ]; then
     mkdir $DIRECTORY
-    cd $DIRECTORY
-    git clone https://github.com/GoblinAlchemist/ldd3
+    cd $DIRECTORY && git clone https://github.com/GoblinAlchemist/ldd3
 fi
-cd ldd3
-cd skull
-pwd
-make
-./scull_load
-cd /dev/
-ls
+
+ls /dev/
+read x
+cd $DIRECTORY/ldd3/skull && make && ./scull_load
+ls /dev/
+
 read x
 
-
-cd ~/
