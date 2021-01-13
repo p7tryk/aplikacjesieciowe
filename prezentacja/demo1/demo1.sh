9ccc2f33720f06a45fa68cdbed4a368a57c6a23c
thisconnect <p7tryk@gmail.com>
2021-01-13 16:02:41 +0100

    demo1 rc2

--- prezentacja/demo1/demo1.sh
+++ prezentacja/demo1/demo1.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+DIRECTORY="demo1"
 
 cd ~/
-mkdir demo1
-cd demo1
-rootdir=`pwd`
-git clone https://github.com/GoblinAlchemist/ldd3
+if [ ! -d "$DIRECTORY" ]; then
+    mkdir $DIRECTORY
+    cd $DIRECTORY
+    git clone https://github.com/GoblinAlchemist/ldd3
+fi
 cd ldd3
 cd skull
+pwd
 make
 ./scull_load
 cd /dev/
@@ -14,5 +17,4 @@ ls
 read x
 
 
-cd $rootdir
-cd ..
+cd ~/
