5b1e1660f085d8bc94f5d0dedc9c1d87297fedc4
thisconnect <p7tryk@gmail.com>
2021-01-13 16:31:01 +0100

    demo1 rc5

--- prezentacja/demo1/demo1.sh
+++ prezentacja/demo1/demo1.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 DIRECTORY="demo1"
 
+sudo apt source linux
+sudo apt install kmod bc cpio flex libelf-dev libssl-dev libncurses5-dev
 
 if [ ! -d "~/$DIRECTORY" ]; then
     mkdir ~/$DIRECTORY
@@ -9,7 +11,7 @@ fi
 
 ls /dev/
 read x
-cd ~/$DIRECTORY/ldd3/skull && make && ./scull_load
+cd ~/$DIRECTORY/ldd3 && make && cd scull && ./scull_load
 ls /dev/
 
 read x
